f49f6b3610a98b4428c37f095bbe7d1351d3d88d
bhushikha <shikhamishrabhu21@gmail.com>
2023-10-03 19:59:50 +0530

    task 17

--- controller/user.js
+++ controller/user.js
@@ -55,7 +55,9 @@
 
 const User = require('../models/users');
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const AWS = require('aws-sdk');
+const Filelink = require('../models/filelink');
 
 function isstringinvalid(string) {
     if (string == undefined || string.length === 0) {
@@ -63,6 +65,39 @@ function isstringinvalid(string) {
     } else {
         return false
     }
+}
+function uploadToS3(data, filename) {
+    const BUCKET_NAME = process.env.BUCKET_NAME;
+    const IAM_USER_KEY = process.env.IAM_USER_KEY;
+    const IAM_USER_SECRET = process.env.IAM_USER_SECRET;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET,
+        // Bucket:BUCKET_NAME
+    })
+    var params = {
+        Bucket: BUCKET_NAME,
+        Key: filename,
+        Body: data,
+        ACL: 'public-read'
+    }
+    return new Promise((resolve, reject) => {
+        s3bucket.upload(params, (err, s3response) => {
+            if (err) {
+                console.log('something went wrong', err)
+                reject(err)
+
+            }
+            else {
+                // console.log('success',s3response)
+                resolve(s3response.Location);
+            }
+        })
+    })
+
+
+
 }
 
 const signup = async (req, res) => {
